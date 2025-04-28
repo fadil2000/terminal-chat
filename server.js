bb2991750a726f15b763392940478ca44d20e7ef
fadil2000 <109853284+fadil2000@users.noreply.github.com>
2025-04-28 23:18:13 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
